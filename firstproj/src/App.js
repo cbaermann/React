81397425fea98aebb451beb735e0610a18d2d51e
Cody Baermann <cbaermann@comcast.net>
2019-10-04 15:51:10 -0700

    initial commit

--- firstproj/src/App.js
+++ firstproj/src/App.js
@@ -44,11 +44,12 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
     };
 
     let persons = null;
@@ -66,16 +67,29 @@ class App extends Component {
           })}
         </div> 
       );
+
+      style.backgroundColor = 'red';
+      
+      
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red'); //classes = ['red']
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold'); //classes = ['red', 'bold']
     }
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle People</button>
-        {persons}
-      </div>
+      
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle People</button>
+          {persons}
+        </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
