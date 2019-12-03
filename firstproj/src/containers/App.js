ada0ea188115c3d0f81bb18545192db41e44e9b2
Cody Baermann <cbaermann@comcast.net>
2019-12-03 06:25:21 -0800

    added WithClass HOC

--- firstproj/src/containers/App.js
+++ firstproj/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../Components/Persons/Persons';
 import Cockpit from '../Components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 
 class App extends Component {
@@ -84,7 +85,7 @@ class App extends Component {
     }
     return (
       
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
           <button 
           onClick={() => {
             this.setState({ showCockpit: false});
@@ -100,7 +101,7 @@ class App extends Component {
           />
           ) : null}
           {persons}
-        </div>
+        </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
--- firstproj/src/hoc/Aux.js
+++ firstproj/src/hoc/Aux.js
@@ -2,3 +2,6 @@
 const aux = props => props.children;
 
 export default aux;
+
+
+//wraps other component. No logic or styling. Just wraps component, maybe adding extra logic at times. This is a Higher Order Component(HOC)
